fd00033dcb65bb4021f9d8e5eef19834fb523911
dmitrii <dmitry-orlov@hotmail.com>
2018-05-05 15:37:55 +0200

    added cors package

--- src/app.js
+++ src/app.js
@@ -2,19 +2,14 @@
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const Router = require('./routes/Router');
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
